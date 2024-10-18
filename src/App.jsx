84e0889b9ecbf7eaea5fcb3368454390e44c7f66
sheila <sheila.fernandez.gonzalez@gmail.com>
2024-10-18 14:28:45 +0200

    Update deleted files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from "styled-components";
-import "./App.css";
 import { theme } from "./theme";
 import { Global } from "./components/myStyledComponents/Global";
 import Header from "./components/Header";
